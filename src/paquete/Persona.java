49211e041c76f55fa6ccd6b6e3022fb303c9d390
dam117 <dam117@PC2017.aulas.local>
2017-05-05 10:53:09 +0200

    3- Ampliación biblioteca

--- src/paquete/Persona.java
+++ src/paquete/Persona.java
@@ -13,7 +13,7 @@ public class Persona {
     String nombre; 
     int edad; 
     
-    public Persona(String nombre, int edad){ //nombre y edad
+    public Persona(String nombre, int edad){
         this.nombre = nombre;
         this.edad = edad;
     }
